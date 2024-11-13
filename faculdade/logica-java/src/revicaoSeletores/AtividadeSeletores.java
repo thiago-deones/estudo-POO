988da2f956283d27c0bc99af53759316dc9cbbe7
Thiago Deones <thiago.deones@gmail.com>
2024-11-12 21:49:00 -0300

    praticando seletores
    Signed-off-by: Thiago Deones <thiago.deones@gmail.com>

--- faculdade/logica-java/src/revicaoSeletores/AtividadeSeletores.java
+++ faculdade/logica-java/src/revicaoSeletores/AtividadeSeletores.java
@@ -42,4 +42,18 @@ public class AtividadeSeletores {
             System.out.print(arr[i] + " |");
         }
     }
+
+    public static void insertionSort(int[] arr) {
+        for (int i = 1; i < arr.length; i++) {
+            int aux = arr[i];
+            for (int j = i - 1; j >= 0 && arr[j] > aux; j--) {
+                arr[j + 1] = arr[j];
+                arr[j] = aux;
+            }
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " |");
+        }
+    }
 }
