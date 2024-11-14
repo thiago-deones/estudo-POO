88eddd0bc1f097a1e734f59f5fffd71cc9b2834b
Thiago Deones <thiago.deones@gmail.com>
2024-11-14 17:27:32 -0300

    atividade criando seletores e medindo tempo
    Signed-off-by: Thiago Deones <thiago.deones@gmail.com>

--- faculdade/logica-java/src/revicaoSeletores/AtividadeSeletores.java
+++ faculdade/logica-java/src/revicaoSeletores/AtividadeSeletores.java
@@ -1,16 +1,44 @@
 package revicaoSeletores;
 
+import java.util.Random;
+import java.util.Scanner;
+
 public class AtividadeSeletores {
     public static void main(String[] args) {
-        int[] arr = {2, 4, 3, 1, 0, -1, 5, 9, 8, 10};
+        Scanner scanner = new Scanner(System.in);
 
-        selectionSort(arr);
-        System.out.println("");
-        bubbleSorte(arr);
+        System.out.println("Digite a semente: ");
+        int semente = scanner.nextInt();
+
+        System.out.println("Digite o tamanho do array:");
+        int size = scanner.nextInt();
+
+        Random random = new Random(semente);
+        int[] arr = new int[size];
+
+        // Geração do array aleatório
+        for (int i = 0; i < size; i++) {
+            arr[i] = random.nextInt(100);
+        }
+
+        System.out.println("\nArray original desordenado:");
+        for (int value : arr) {
+            System.out.print(value + "| ");
+        }
+        System.out.println("\n");
+
+
+        selectionSort(arr.clone());
+        bubbleSort(arr.clone());
+        insertionSort(arr.clone());
+        mergeSort(arr.clone());
     }
 
     public static void selectionSort(int[] arr) {
-        for (int i = 0; i < arr.length; i++){
+        System.out.println("Selection Sort:");
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < arr.length; i++) {
             int posicao = i;
             for (int j = i + 1; j < arr.length; j++) {
                 if (arr[posicao] > arr[j]) {
@@ -22,38 +50,125 @@ public class AtividadeSeletores {
             arr[posicao] = aux;
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " |");
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+
+        for (int value : arr) {
+            System.out.print(value + "| ");
         }
+        System.out.println("\nTempo de execução: " + duration + " nanosegundos\n");
     }
 
-    public static void bubbleSorte(int[] arr) {
-        for (int i = 0; i < arr.length; i++) {
-            for (int j = 1 + i; j < arr.length; j++) {
-                if (arr[i] > arr[j]){
-                    int aux = arr[i];
-                    arr[i] = arr[j];
-                    arr[j] = aux;
+    public static void bubbleSort(int[] arr) {
+        System.out.println("Bubble Sort:");
+        long startTime = System.nanoTime();
+
+        for (int i = 0; i < arr.length - 1; i++) {
+            for (int j = 0; j < arr.length - i - 1; j++) {
+                if (arr[j] > arr[j + 1]) {
+                    int aux = arr[j];
+                    arr[j] = arr[j + 1];
+                    arr[j + 1] = aux;
                 }
             }
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " |");
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+
+        for (int value : arr) {
+            System.out.print(value + "| ");
         }
+        System.out.println("\nTempo de execução: " + duration + " nanosegundos\n");
     }
 
     public static void insertionSort(int[] arr) {
+        System.out.println("Insertion Sort:");
+        long startTime = System.nanoTime();
+
         for (int i = 1; i < arr.length; i++) {
             int aux = arr[i];
-            for (int j = i - 1; j >= 0 && arr[j] > aux; j--) {
+            int j = i - 1;
+            while (j >= 0 && arr[j] > aux) {
                 arr[j + 1] = arr[j];
-                arr[j] = aux;
+                j--;
             }
+            arr[j + 1] = aux;
         }
 
-        for (int i = 0; i < arr.length; i++) {
-            System.out.print(arr[i] + " |");
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+
+        for (int value : arr) {
+            System.out.print(value + "| ");
+        }
+        System.out.println("\nTempo de execução: " + duration + " nanosegundos\n");
+    }
+
+    public static void mergeSort(int[] arr) {
+        System.out.println("Merge Sort:");
+        long startTime = System.nanoTime();
+
+        mergeSortRecursive(arr, 0, arr.length - 1);
+
+        long endTime = System.nanoTime();
+        long duration = endTime - startTime;
+
+        for (int value : arr) {
+            System.out.print(value + "| ");
+        }
+        System.out.println("\nTempo de execução: " + duration + " nanosegundos\n");
+    }
+
+    private static void mergeSortRecursive(int[] arr, int left, int right) {
+        if (left < right) {
+            int middle = (left + right) / 2;
+
+            mergeSortRecursive(arr, left, middle);
+            mergeSortRecursive(arr, middle + 1, right);
+
+            merge(arr, left, middle, right);
+        }
+    }
+
+    private static void merge(int[] arr, int left, int middle, int right) {
+        int n1 = middle - left + 1;
+        int n2 = right - middle;
+
+        int[] leftArray = new int[n1];
+        int[] rightArray = new int[n2];
+
+        for (int i = 0; i < n1; i++) {
+            leftArray[i] = arr[left + i];
+        }
+        for (int j = 0; j < n2; j++) {
+            rightArray[j] = arr[middle + 1 + j];
+        }
+
+        int i = 0, j = 0;
+        int k = left;
+
+        while (i < n1 && j < n2) {
+            if (leftArray[i] <= rightArray[j]) {
+                arr[k] = leftArray[i];
+                i++;
+            } else {
+                arr[k] = rightArray[j];
+                j++;
+            }
+            k++;
+        }
+
+        while (i < n1) {
+            arr[k] = leftArray[i];
+            i++;
+            k++;
+        }
+
+        while (j < n2) {
+            arr[k] = rightArray[j];
+            j++;
+            k++;
         }
     }
 }
