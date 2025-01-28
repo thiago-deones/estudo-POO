c45b19dc0da487a6f956bf4c93314554f0013ae6
thiago deones <thiago.deones@gmail.com>
2025-01-28 17:25:09 -0300

    criando uma busca atraves da API do google livros
    Signed-off-by: thiago deones <thiago.deones@gmail.com>

---
+++ faculdade/logica-java/src/googleBooks/Principal.java
@@ -0,0 +1,14 @@
+package googleBooks;
+
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+        Scanner scan = new Scanner(System.in);
+
+        System.out.println("Digite o livro que você deseja pesquisar");
+        var busca = scan.nextLine();
+
+        https://www.google.com/search?udm=36&q=Fausto
+    }
+}
