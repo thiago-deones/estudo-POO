c76bce7b28bbc82c0e037c1cc41f481dc832afe5
thiago deones <thiago.deones@gmail.com>
2025-01-28 17:44:23 -0300

    projeto agarrado por falta de liberação API google
    Signed-off-by: thiago deones <thiago.deones@gmail.com>

--- faculdade/logica-java/src/googleBooks/Principal.java
+++ faculdade/logica-java/src/googleBooks/Principal.java
@@ -1,14 +1,30 @@
 package googleBooks;
 
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
 import java.util.Scanner;
 
 public class Principal {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         Scanner scan = new Scanner(System.in);
 
         System.out.println("Digite o livro que você deseja pesquisar");
-        var busca = scan.nextLine();
+        var tituloBusca = scan.nextLine();
 
-        https://www.google.com/search?udm=36&q=Fausto
+        String chave= "Esperanado chave fornecida pelo google";
+        String endereco = "https://www.googleapis.com/books/v1/volumes?q=" + tituloBusca + "&key=" + chave;
+
+        HttpClient client = HttpClient.newHttpClient();
+
+        HttpRequest request = HttpRequest.newBuilder()
+                .uri(URI.create("https://jsonplaceholder.typicode.com/posts"))
+                .GET()
+                .build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
     }
 }
