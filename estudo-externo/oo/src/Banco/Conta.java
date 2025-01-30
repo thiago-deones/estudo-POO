5a0919ea5c92d4d80e967fb57a1a6541b8b5bf97
thiago deones <thiago.deones@gmail.com>
2025-01-30 19:15:52 -0300

    projeto final bytebanco
    Signed-off-by: thiago deones <thiago.deones@gmail.com>

--- estudo-externo/oo/src/Banco/Conta.java
+++ estudo-externo/oo/src/Banco/Conta.java
@@ -1,13 +1,41 @@
 package Banco;
 
 public class Conta {
-    double saldo;
-    int agencia;
-    int numero;
-    String titular;
+    private double saldo;
+    private int agencia;
+    private int numero;
+    Cliente titular;
 
     public void deposita(double valor) {
         this.saldo += valor;
+    }
+
+    public int getAgencia() {
+        return agencia;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setSaldo(double saldo) {
+        this.saldo = saldo;
+    }
+
+    public void setAgencia(int agencia) {
+        if (agencia < 0) {
+            System.out.println("Não é pérmitido numero negativo");
+        return;
+        }
+        this.agencia = agencia;
+    }
+
+    public void setNumero(int numero) {
+        if (numero < 0) {
+            System.out.println("Não é pérmitido numero negativo");
+            return;
+        }
+        this.numero = numero;
     }
 
     public boolean saca(double valor) {
@@ -19,6 +47,24 @@ public class Conta {
         }
     }
 
+    public boolean transfere(double valor, Conta destino) {
+        if (this.saldo >= valor) {
+            this.saldo -= valor;
+            destino.deposita(valor);
+            return true;
+        }
+        return false;
+    }
 
+    public double getSaldo() {
+        return this.saldo;
+    }
 
+    public void setTitular(Cliente titular) {
+        this.titular = titular;
+    }
+
+    public Cliente getTitular() {
+        return titular;
+    }
 }
--- estudo-externo/oo/src/Banco/Principal.java
+++ estudo-externo/oo/src/Banco/Principal.java
@@ -4,7 +4,7 @@ public class Principal {
     public static void main(String[] args) {
         Conta primeiraConta = new Conta();
 
-        primeiraConta.saldo = 200;
-        System.out.println(primeiraConta.saldo);
+
+
     }
 }
