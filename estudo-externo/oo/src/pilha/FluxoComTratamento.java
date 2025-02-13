1285d4d8568335e92c563feb0b4d3fb3d7f1ec8e
thiago deones <thiago.deones@gmail.com>
2025-02-13 17:52:31 -0300

    Criando minha propri exception
    Signed-off-by: thiago deones <thiago.deones@gmail.com>

---
+++ estudo-externo/oo/src/pilha/FluxoComTratamento.java
@@ -0,0 +1,23 @@
+package pilha;
+
+public class FluxoComTratamento {
+    public static void main(String[] args)  {
+        System.out.printf("Ini do main");
+        metodo1();
+        System.out.println("Fim do main");
+    }
+
+    private static void metodo1() {
+        System.out.println("Ini do metodo1");
+        metodo2();
+        System.out.println("Fim do metodo1");
+    }
+
+    private static void metodo2() {
+        System.out.println("Ini do metodo2");
+
+        throw new ArithmeticException(" Deu erro");
+
+//        System.out.println("Fim do metodo2");
+    }
+}
