ab18113e6e050be113c6573099c45d2cc411da6c
thiago deones <thiago.deones@gmail.com>
2025-02-14 18:13:53 -0300

    praticando mais metodos do pacote lan, atraves da class String
    Signed-off-by: thiago deones <thiago.deones@gmail.com>

--- estudo-externo/oo/src/ConhecendoLang/TesteString.java
+++ estudo-externo/oo/src/ConhecendoLang/TesteString.java
@@ -4,8 +4,16 @@ public class TesteString {
 
     public static void main(String[] args) {
 
-        String nome = "Alura";
+        String nome = "Alura";// objeto literal
+        // String outro = new String("Alura");
 
+        String outra = nome.replace("A", "a");
+
+        System.out.println(outra);
+
+        String outra1 = nome.toLowerCase();
+
+        System.out.println(outra1);
 
     }
 }
