c2a4dbf9e478dd30cf968d926155b3a87f934ac1
Thiago <115191028+thiago-deones@users.noreply.github.com>
2026-09-02 13:09:44 -0300

    finalizando polimorfismos

---
+++ estudo-externo/java/objeto/src/javacore/Npolimorfismo/servico/RepositorioBancoDeDados.java
@@ -0,0 +1,11 @@
+package javacore.Npolimorfismo.servico;
+
+import javacore.Npolimorfismo.repositorio.Repositorio;
+
+public class RepositorioBancoDeDados implements Repositorio {
+
+    @Override
+    public void salvar() {
+        System.out.println("SAlvando no banco de dados");
+    }
+}
---
+++ estudo-externo/java/objeto/src/javacore/Npolimorfismo/test/TestRepositorio01.java
@@ -0,0 +1,10 @@
+package javacore.Npolimorfismo.test;
+
+import javacore.Npolimorfismo.repositorio.Repositorio;
+import javacore.Npolimorfismo.servico.RepositorioBancoDeDados;
+
+public class TestRepositorio01 {
+    public static void main(String[] args) {
+        Repositorio repositorioBancoDeDados = new RepositorioBancoDeDados();
+    }
+}
