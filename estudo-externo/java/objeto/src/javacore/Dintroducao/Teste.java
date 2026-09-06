887d014bb94acdd8bc8a27729bdeb16e4bd7420e
Thiago <115191028+thiago-deones@users.noreply.github.com>
2026-09-06 10:14:59 -0300

    enviando exercicio do beecrowd

--- estudo-externo/java/objeto/src/javacore/Dintroducao/Teste.java
+++ estudo-externo/java/objeto/src/javacore/Dintroducao/Teste.java
@@ -7,15 +7,6 @@ public class Teste {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        int dias = input.nextInt();
-        int anos = dias/365;
-        int resto = dias%365;
-        int meses = resto/30;
-        int dia = resto%30;
-        System.out.printf("%d ano(s)%n", anos);
-        System.out.printf("%d mes(es)%n", meses);
-        System.out.printf("%d dia(s)%n", dia);
-
         input.close();
        }
 
